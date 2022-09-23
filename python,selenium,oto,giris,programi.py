ab94b0d669479cbe5633d74d0797f9a4af419805
enne <52035806+ensoioffical@users.noreply.github.com>
2022-09-23 15:32:36 +0300

    Update python,selenium,oto,giris,programi.py

--- python,selenium,oto,giris,programi.py
+++ python,selenium,oto,giris,programi.py
@@ -1,16 +1,28 @@
-tk="üİçğşöı"
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
+browserr = webdriver.Chrome()
 
-parola = input("parola girin : ")
+browserr.get("https://www.instagram.com/?hl=tr")
 
-for i in parola:
-    if i in tk:
-        raise TypeError("hatalı parola")
-    else:
-        pass
-print("geçerli parola")        
+time.sleep(5)
 
+name = browserr.find_element(By.NAME,("username"))
+name.click()
 
+name.send_keys("username")
+
+time.sleep(2)
+
+name2 = browserr.find_element(By.NAME,("password"))
+name2.click()
+name2.send_keys("password")
+
+time.sleep(2)
+
+name3 = browserr.find_element(By.XPATH,("/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button/div"))
+name3.click()
 
 
     
